6d7a8ba3eaae66b64780873720bc357faaf7eee2
chrissagg <chriss.agg@outlook.com>
2020-10-02 11:42:39 -0300

    Se establece la version 1.1.0

--- js/js1.js
+++ js/js1.js
@@ -8,8 +8,21 @@ function redirect2(onClick){
     window.open("https://www.instagram.com/itecharg.shop/")
 }
 
+function redirectNav(onClick){
+    window.open("https://www.instagram.com/chriss.agg/")
+}
+
 //NAV ANIMATION
 window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+/*
+const hamburguer = document.getElementById('hamburguer');
+const navUL = document.getElementById('nav-ul');
+
+hamburguer.addEventListener('click', () => {
+    navUL.classList.toogle('show');
+});
+*/
